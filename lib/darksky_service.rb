07e21efce28db52b8b57aacccbabb2d3ab7267a3
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-26 11:23:04 -0600

    creates darksky service

---
+++ lib/darksky_service.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+require 'json'
+
+class DarkskyService
+  attr_reader :coordinates, :key
+
+  def initialize(coordinates)
+    @coordinates = coordinates
+    @key = '640644acf3d2e971e41cef9646c4f0aa'
+  end
+
+  def get_forecast
+    response = Net::HTTP.get(URI "https://api.darksky.net/forecast/#{key}/#{coordinates[:lat]},#{coordinates[:lng]}")  
+    JSON.parse(response, symbolize_names: true)
+  end
+end
--- lib/forecaster.rb
+++ lib/forecaster.rb
@@ -1,24 +1,38 @@
 require 'pry'
 require "./lib/zipcode_service.rb"
+require "./lib/darksky_service.rb"
+# require "./lib/weather_formatter.rb"
 
 class Forecaster
-  attr_reader :zipcode, :granularity, :output, :coordinates
+  attr_reader :zipcode, :granularity, :output
   def initialize(args)
     @zipcode = args[0] || nil
     @granularity = args[1] || nil
     @output = args[2] || nil
   end
 
-  def get_coordinates
+  def retrieve_coordinates
     zcs = ZipcodeService.new(zipcode)
-    @coordinates ||= zcs.get_lat_lng
+    zcs.get_lat_lng
   end
 
+  def retrieve_forecast
+    coordinates = retrieve_coordinates
+    dss = DarkskyService.new(coordinates)
+    dss.get_forecast
+  end
+
+  def print_weather
+    forecast = retrieve_forecast
+    formatter = WeatherFormatter.new(granularity, forecast)
+    formatted = formatter.format_weather
+    puts formatted
+  end
   
 end
 
 ######
 forecast = Forecaster.new(ARGV)
-forecast.get_coordinates
-puts forecast.coordinates
+forecast.print_weather
+
 
